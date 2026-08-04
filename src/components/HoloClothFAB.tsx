0b47fc1c08439b2ada077c847ce7234ae4102462
Chundong Wang <cd@ottotheagent.com>
2026-08-03 23:19:54 -0500

    Change the holo-cloth FAB emoji to 🪞

--- src/components/HoloClothFAB.tsx
+++ src/components/HoloClothFAB.tsx
@@ -24,7 +24,7 @@ export default function HoloClothFAB() {
         aria-label={t.fabLabel}
         className="w-14 h-14 rounded-full shadow-lg active:scale-95 transition-transform flex items-center justify-center bg-[#2B4B8C] hover:bg-[#1e3a70]"
       >
-        <span className="text-2xl">🪩</span>
+        <span className="text-2xl">🪞</span>
       </button>
 
       {open && <HoloClothModal onClose={() => setOpen(false)} closeLabel={t.close} />}
